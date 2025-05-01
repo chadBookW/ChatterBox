8107726ffa7a1309ff8d6cf0a20498b450071bb8
Nipun <nipunmanavya13@gmail.com>
2025-05-01 17:53:42 +0530

    Project ChatApp

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useAuthStore } from "../store/useAuthStore";
-import { LogOut, MessageSquare, Settings, User } from "lucide-react";
+import { LogOut, Home, Settings, User } from "lucide-react"; // changed MessageSquare to Home
 
 const Navbar = () => {
   const { logout, authUser } = useAuthStore();
@@ -15,9 +15,9 @@ const Navbar = () => {
           <div className="flex items-center gap-8">
             <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
-                <MessageSquare className="w-5 h-5 text-primary" />
+                <Home className="w-5 h-5 text-primary" /> {/* icon changed here */}
               </div>
-              <h1 className="text-lg font-bold">Chatty</h1>
+              <h1 className="text-lg font-bold">ChatterBox</h1>
             </Link>
           </div>
 
@@ -52,4 +52,5 @@ const Navbar = () => {
     </header>
   );
 };
+
 export default Navbar;
